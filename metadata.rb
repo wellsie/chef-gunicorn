db330c847718a4eb67a01ceb0e364cf55857fd57
Jonathan Wells <jono@whitesky.com>
2014-07-28 08:04:59 -0700

    lintting rubocop suggestions

--- metadata.rb
+++ metadata.rb
@@ -1,9 +1,9 @@
-name             "gunicorn"
-maintainer       "Jono Wells"
-maintainer_email "7@oj.io"
-license          "Apache 2.0"
-description      "Installs/Configures Gunicorn"
+name 'gunicorn'
+maintainer 'Jono Wells'
+maintainer_email '7@oj.io'
+license 'Apache 2.0'
+description 'Installs/Configures Gunicorn'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "2.0.0"
+version '2.0.0'
 
-depends "python"
+depends 'python'
