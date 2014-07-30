555059bb98a4320a234ac0e903a0a8dafe874c58
Jonathan Wells <jono@whitesky.com>
2014-07-30 10:05:54 -0700

    removed comments from default_test

--- test/cookbooks/gunicorn_test/files/default/test/default_test.rb
+++ test/cookbooks/gunicorn_test/files/default/test/default_test.rb
@@ -4,10 +4,6 @@ require File.expand_path('helpers', File.dirname(__FILE__))
 # For your own cookbooks, describe mycookbookname::default
 describe 'gunicorn::default' do
 
-  # Helpers::MinitestHandler library is defined at
-  # #{cookbook_root}/files/default/tests/minitest/support/helpers.rb
-  # For each cookbook, rename library to Helpers::MyCookbookName in
-  # this file and in helpers.rb
   include Helpers::GunicornTest
 
   gunicorn = Mixlib::ShellOut.new('gunicorn -v')
